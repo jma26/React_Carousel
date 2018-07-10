3a77e1d0c8deb76250cba30ee709f11e0ffd7641
Jesse Ma <j.masonoma17@gmail.com>
2018-07-10 01:17:42 -0700

    Initial commit

---
+++ badminton/src/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header-container">
+                <h1 className='headline'> Welcome, Badminton Fanatics! </h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ badminton/src/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './home.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="home-container">
+                <h1 className="home-headline"> Select a player below! </h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
